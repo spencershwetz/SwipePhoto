8270bfec190e377981850e35a05e210cbf973dfe
Spencer <spencershwetz@gmail.com>
2024-02-13 10:40:43 -0500

    Initial Commit

---
+++ SwipePhoto/SwipePhotoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwipePhotoApp.swift
+//  SwipePhoto
+//
+//  Created by spencer on 2/13/24.
+//
+
+import SwiftUI
+
+@main
+struct SwipePhotoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
